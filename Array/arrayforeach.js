58f8f0bb891ace40f38b889815fa22ac7319db46
ddas4548 <ddas4548@gmail.com>
2024-04-26 16:58:59 +0530

    array for each method

---
+++ Array/arrayforeach.js
@@ -0,0 +1,11 @@
+let nums =new Array(12,13,14,15)
+console.log("printing orginal array")
+nums. forEach(element => {
+    console.log(element)
+});
+nums.reverse();
+console.log("printing reverse array")
+
+nums.forEach(element => {
+    console.log(element)
+});
