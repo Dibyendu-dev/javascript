4eaafd33d9245af9db897abbf465d45a2afd9806
ddas4548 <ddas4548@gmail.com>
2024-04-26 12:52:39 +0530

    add a comment for filter method

--- Array/arrayfilter.js
+++ Array/arrayfilter.js
@@ -4,5 +4,5 @@ function isBigEnough(e){
 let passed =[12,5,8,130,44].filter(isBigEnough);
 console.log("Test value : " + passed);
 
-let passed2 =[12,5,8,130,44].filter((e)=>(e>=10));
+let passed2 =[12,5,8,130,44].filter((e)=>(e>=10)); // for one line statement u can use explicit return using ()
 console.log("Test value : " + passed);
