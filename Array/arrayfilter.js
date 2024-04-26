553274b1d60601f6438f9dae02c566db6db29e80
Dibyendu Das <98630356+Dibyendu-dev@users.noreply.github.com>
2024-04-26 07:18:13 +0000

    arrow function filter method added

--- Array/arrayfilter.js
+++ Array/arrayfilter.js
@@ -1,5 +1,8 @@
-function isBigEnough(element){
-    return(element>=10)
+function isBigEnough(e){
+    return(e>=10)
 }
 let passed =[12,5,8,130,44].filter(isBigEnough);
+console.log("Test value : " + passed);
+
+let passed2 =[12,5,8,130,44].filter((e)=>(e>=10));
 console.log("Test value : " + passed);
